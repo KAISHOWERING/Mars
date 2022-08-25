b99949c75d5ec3b6104eb5e8bd1280ea5549e846
KAISHOWERING <785780652@qq.com>
2022-08-25 19:31:48 +0800

    ADD SIMULATION TOOL

--- dont_use/dont_use.tcl
+++
@@ -1,8 +0,0 @@
-# set_dont_use  [get_lib_cells {scc40nll_vhsc40_lvt_tt_v1p2_25c_ccs/CLK*}]
-# get_attribute [get_lib_cells {scc40nll_vhsc40_lvt_tt_v1p2_25c_ccs/CLK*}] dont_use
-
-# set_dont_use  [get_lib_cells {scc40nll_vhsc40_lvt_tt_v1p2_25c_ccs/*V0*}]
-# get_attribute [get_lib_cells {scc40nll_vhsc40_lvt_tt_v1p2_25c_ccs/*V0*}] dont_use
-
-# set_dont_use  [get_lib_cells {scc40nll_vhsc40_lvt_tt_v1p2_25c_ccs/*V1*}]
-# get_attribute [get_lib_cells {scc40nll_vhsc40_lvt_tt_v1p2_25c_ccs/*V1*}] dont_use
